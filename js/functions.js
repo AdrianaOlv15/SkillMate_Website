f1d512e8f03086aa4452555d72924d0285f99435
AdrianaOlv15 <aolver6@uic.edu>
2023-04-02 14:04:46 -0500

    pushing html file

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,62 @@
+// firebase analytics 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+//import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC_jgj9zfAr5KIVQXlKPXby5Od2BOaTnGM",
+//   authDomain: "rpi-mlx90614-821e6.firebaseapp.com",
+//   databaseURL: "https://rpi-mlx90614-821e6-default-rtdb.firebaseio.com",
+//   projectId: "rpi-mlx90614-821e6",
+//   storageBucket: "rpi-mlx90614-821e6.appspot.com",
+//   messagingSenderId: "213693056033",
+//   appId: "1:213693056033:web:cca76584faaa68919b0f60",
+//   measurementId: "G-Q5NXQZZXN4"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// //import{getDatabase, ref, child, onValue, get} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+
+// const db = getDatabase();
+
+
+// function GetDataRealTime(){
+
+//}
 
  // Array of Title for Skill Pages 
-let skillTitlesArray = ["3D PRINTING", "SOLDERING", "PROGRAMMING", 
-                        "GRAPHIC DESIGN", "PHOTO/VIDEO EDITING", "3D MODELING", 
-                        "TIME MANAGEMENT", "LANGUAGES", "MARKETING", 
-                        "REMOTE MEETING\nTROUBLESHOOT"];
+let skillTitlesArray = ["3D PRINTING", "SOLDERING", "PROGRAMMING", "GRAPHIC DESIGN", 
+                        "PHOTO/VIDEO EDITING", "3D MODELING", "TIME MANAGEMENT", "LANGUAGES",
+                        "MARKETING", "REMOTE MEETING\nTROUBLESHOOT"];
 
 // ppl labels that will be populated on the Skill Pages       
-let spacesArray = ["p0_space", "p1_space", "p2_space",
-                     "p3_space", "p4_space", "p5_space",
-                     "p6_space", "p7_space", "p8_space",
-                     "p9_space"];
+let spacesArray = ["p0_space", "p1_space", "p2_space", "p3_space", "p4_space", "p5_space",
+                   "p6_space", "p7_space", "p8_space", "p9_space"];
 
 
-let statusArray = ["p0_status", "p1_status", "p2_status",
-                     "p3_status", "p4_status", "p5_status",
-                     "p6_status", "p7_status", "p8_status",
-                     "p9_status"];                 
+let statusArray = ["p0_status", "p1_status", "p2_status", "p3_status", "p4_status", "p5_status",
+                   "p6_status", "p7_status", "p8_status","p9_status"];                 
 
 // Person Object                
-function Person(first, last, org, print3d, solder, prog, graph, photo, model3d, time, lang, market, remote, stat) {
+function Person(first, last, org, print3d, solder, prog, graph, photo, model3d, time, 
+                lang, market, remote, stat) {
+    
+    // connectory status
     this.status = stat;
-  
     // personal info
     this.firstName = first;
     this.lastName = last;
+    this.picture = "../images/empty.jpeg";
     this.orgName = org;
     // contact info
     this.email = "none";
@@ -42,50 +74,52 @@ function Person(first, last, org, print3d, solder, prog, graph, photo, model3d, 
     this.marketing = market;    // 9
     this.remoteMeet = remote;   // 10 
     // skills descriptions
-    this.printing3D_descr =       "empty"; // #1
-    this.soldering_descr =        "empty"; // #2
-    this.programing_descr =       "empty"; // #3
-    this.graphicDesign_descr =    "empty"; // #4
-    this.photoVideo_descr =       "empty"; // #5
-    this.modeling3D_descr =       "empty"; // #6
-    this.timeManagement_descr =   "empty"; // #7
-    this.languages_descr =        "empty"; // #8
-    this.marketing_descr =        "empty"; // #9
-    this.remoteMeet_descr =       "empty"; // #10
-
-
+    this.printing3D_descr = "description for 3D printing skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";     
+    this.soldering_descr = "description for soldering skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";    
+    this.programing_descr = "description for programming skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";      
+    this.graphicDesign_descr = "description for graphic design skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";      
+    this.photoVideo_descr = "description for photo and video skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";      
+    this.modeling3D_descr = "description for 3D modeling skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";      
+    this.timeManagement_descr = "description for time management skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";      
+    this.languages_descr = "description for languages skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";      
+    this.marketing_descr = "description for marketing skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";      
+    this.remoteMeet_descr = "description for remote meeting skills goes here _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";            
+    
     // methods to write skill descriptions
     this.addDescription = function (skillNum, description) {
-        if (skillNum == 1){
-          this.printing3D_descr =   description; // #1
-        } else if (skillNum == 2){
-          this.soldering_descr =    description; // #2
-        } else if (skillNum == 3){
-          this.programing_descr =   description; // #3
-        } else if (skillNum == 4){
-          this.graphicDesign_descr = description; // #4
-        } else if (skillNum == 5){
-          this.photoVideo_descr =   description; // #5
-        } else if (skillNum == 6){
-          this.modeling3D_descr =   description; // #6
-        } else if (skillNum == 7){
-          this.timeManagement_descr = description; // #7
-        } else if (skillNum == 8){
-          this.languages_descr =    description; // #8
-        } else if (skillNum == 9){
-          this.marketing_descr =    description; // #9
-        } else if (skillNum == 10){
-          this.remoteMeet_descr =    description; // #10
-        }
+        if (skillNum == 1){ this.printing3D_descr = description;} // #1 
+        else if (skillNum == 2){ this.soldering_descr = description;} // #2
+        else if (skillNum == 3){ this.programing_descr = description;} // #3
+        else if (skillNum == 4){ this.graphicDesign_descr = description;} // #4
+        else if (skillNum == 5){ this.photoVideo_descr = description;} // #5
+        else if (skillNum == 6){ this.modeling3D_descr = description;} // #6
+        else if (skillNum == 7){ this.timeManagement_descr = description;} // #7
+        else if (skillNum == 8){ this.languages_descr = description;} // #8
+        else if (skillNum == 9){ this.marketing_descr = description;} // #9
+        else if (skillNum == 10){ this.remoteMeet_descr = description;} // #10
     }
-    
+ 
     // methods to write contact info
     this.addEmail = function (input) { this.email = input;}
     this.addPhone = function (input) { this.phoneNumber = input; }
     this.addSocial = function (input) { this.socialMedia = input; }
+    this.addPicture = function (input){ this.picture = input;}
+
+    this.getSkill = function(skillNum){
+      if (skillNum == 1){ return this.printing3D_descr; }// #1
+      else if (skillNum == 2){ return this.soldering_descr; }// #2
+      else if (skillNum == 3){ return this.programing_descr; }// #3
+      else if (skillNum == 4){ return this.graphicDesign_descr; }// #4
+      else if (skillNum == 5){ return this.photoVideo_descr; }// #5
+      else if (skillNum == 6){ return this.modeling3D_descr; }// #6
+      else if (skillNum == 7){ return this.timeManagement_descr; }// #7
+      else if (skillNum == 8){ return this.languages_descr; }// #8
+      else if (skillNum == 9){ return this.marketing_descr; }// #9
+      else if (skillNum == 10){ return this.remoteMeet_descr; }// #10
+    }
 }
 
-// 9 People Object to create mock data! 
+// 9 People Objects to create mock data! 
 let person0 = new Person("Kenny", "Ackerman",   "Survey Corps",         0, 0, 1, 1, 1, 0, 1, 1, 1, 1,1);
 let person1 = new Person("Adriana", "Olvera",   "NVIDIA Industries",    1, 0, 1, 1, 1, 0, 0, 1, 1, 0,0);
 let person2 = new Person("Mira", "Sweis",       "Microsoft",            1, 0, 0, 1, 1, 1, 0, 1, 1, 0,1);
@@ -97,8 +131,19 @@ let person7 = new Person("Mike", "Zacharias",   "OnShape Inc.",         0, 1, 1,
 let person8 = new Person("Viktor", "Nikiforov", "Ekotrope Laboratory",  0, 1, 1, 1, 0, 0, 1, 1, 1, 1,1);
 let peopleArray = [person0, person1, person2, person3, person4, person5, person6, person7, person8];
 
+// Adding profile pictures to each people
+person0.addPicture("../images/kenny.PNG");
+person1.addPicture("../images/adriana.PNG");
+person2.addPicture("../images/mira.PNG");
+person3.addPicture("../images/jazmin.PNG");
+person4.addPicture("../images/alejandro.PNG");
+person5.addPicture("../images/labiba.PNG");
+person6.addPicture("../images/erwin.PNG");
+person7.addPicture("../images/mike.PNG");
+person8.addPicture("../images/viktor.PNG");
 
-// MOVING THROUGH PAGES /////////////////////////////////////////////////////////////////////////////    
+// MOVING FORWARD THRU PAGES ///////////////////////////////////////////////////////////////////////    
+
 // From Home --> Skill Menu
 function start(){
   window.location.replace('Pages/skills_menu.html');
@@ -107,30 +152,39 @@ function start(){
 function setSkill_moveSkillPage (skillNum){
   sessionStorage.setItem("Skill_Title", skillTitlesArray[skillNum]);
   sessionStorage.setItem("Skill_Num", skillNum);
-  window.location.replace('skill_page.html');
+  window.location.replace('skill_page.html'); // page change
 }
-// From Skill Menu --> Home
-function backToWelcome(){
-  window.location.replace('../index.html');
+// From Skill Page --> Person's Page
+function movePersonPage(tagName){
+  element = document.getElementById(tagName);
+  let name = element.innerText;
+  sessionStorage.setItem("Person_Name",name);
+  window.location.replace('person_page.html'); // page change
+}
+// MOVING BACK THRU PAGES ///////////////////////////////////////////////////////////////////////    
+
+// From Person's Page --> Skill Page
+function backToSkillsPage(){
+  window.location.replace('skill_page.html'); // page change
 }
 // From Skill Page --> Skill Menu
 function backToSkillsMenu(){
-  window.location.replace('skills_menu.html');
+  window.location.replace('skills_menu.html'); // page change
 }
-
-function movePersonPage(personNum){
-  sessionStorage.setItem("Person_Num",personNum);
-  window.location.replace('person_page.html');
+// From Skill Menu --> Home
+function backToWelcome(){
+  window.location.replace('../index.html'); // page change
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 function updateSkillPage () {
-
+  // log to check right skill
   console.log("Skill Selected: " + sessionStorage.getItem("Skill_Title"));
-
+  // update skill title
   title_obj = document.getElementById("pageTitle"); // get title object using its ID
   title_obj.innerText = sessionStorage.getItem("Skill_Title");
+  // update people list
   setPeopleList();
 }
 
@@ -144,35 +198,42 @@ function setPeopleList(){
 
   for(var i = 0; i < peopleArray.length; i++){ // go through the people in the array
     
-    console.log( i + "# "  + peopleArray[i].firstName + "has Skill? -> " + checkSkill(peopleArray[i],skillNum));
+    console.log( i + "# "  + peopleArray[i].firstName + " has Skill? -> " + checkSkill(peopleArray[i],skillNum));
 
     if (checkSkill(peopleArray[i],skillNum) == "YES"){
       space_obj = document.getElementById(spacesArray[space]);
       space_obj.innerText = peopleArray[i].firstName + " " + peopleArray[i].lastName;
       // STYLE
       space_obj.style.color = 'white';
-      space_obj.style.background = 'rgb(0, 1, 88)'
+      space_obj.style.background = 'rgb(0, 1, 88)';
       space_obj.style.width = '200px';
       space_obj.style.fontSize = '20px'
+      space_obj.style.borderStyle = 'solid'; 
+      space_obj.style.borderColor = 'rgb(0, 1, 88)';
 
-
+      // Connectory Status Tag Set Up //////////////////////////////////////////////
       space_obj2 = document.getElementById(statusArray[space]);
-      // STYLE
+      
+      // Making Letters White
       space_obj2.style.color = 'white';
       space_obj2.style.width = '100px';
       space_obj2.style.fontSize = '20px'
 
+      // Displaying Connectory Status (Red / Green)
       if (peopleArray[i].status == 1){
-        space_obj2.style.background = 'rgb(40, 141, 0)'
+        space_obj2.style.background = 'rgb(40, 141, 0)';
         space_obj2.innerText = "IN";
+        space_obj2.style.borderStyle = 'solid'; 
+        space_obj2.style.borderColor = 'rgb(40, 141, 0)';
       } else {
-        space_obj2.style.background = 'rgb(208, 0, 0)'
+        space_obj2.style.background = 'rgb(208, 0, 0)';
         space_obj2.innerText = "OUT";
+        space_obj2.style.borderStyle = 'solid'; 
+        space_obj2.style.borderColor = 'rgb(208, 0, 0)';
       }
-
+      // Move to next person ///////////////////////////////////////////////////////
       space = space + 1;
     }
-
   }
 
   for (var j = space; j < spacesArray.length-1; j++){
@@ -190,6 +251,7 @@ function setPeopleList(){
   }
 }
 
+// Function to check if a person has a specific skill
 function checkSkill (person, skillNum){
   
   hasSkill = "NO";
@@ -212,20 +274,59 @@ function checkSkill (person, skillNum){
 
 
 
+function loadProfile(){
+ 
+  // get the name of the person we selected
+  text = sessionStorage.getItem("Person_Name");
+  console.log( "Saved Name:" + text)
+  
+  // find the person's object
+  if (sessionStorage.getItem("Person_Name") == "Kenny Ackerman"){ personPicked = person0; console.log("showing 0") }
+  if (sessionStorage.getItem("Person_Name") == "Adriana Olvera"){ personPicked = person1; console.log("showing 1") }
+  if (sessionStorage.getItem("Person_Name") == "Mira Sweis"){ personPicked = person2; console.log("showing 2") }
+  if (sessionStorage.getItem("Person_Name") == "Jazmin Santoyo"){ personPicked = person3; console.log("showing 3") }
+  if (sessionStorage.getItem("Person_Name") == "Alejandro Perez"){ personPicked = person4; console.log("showing 4") }
+  if (sessionStorage.getItem("Person_Name") == "Labiba Hussien"){ personPicked = person5; console.log("showing 5") }
+  if (sessionStorage.getItem("Person_Name") == "Erwin Smith"){ personPicked = person6; console.log("showing 6")}
+  if (sessionStorage.getItem("Person_Name") == "Mike Zacharias"){ personPicked = person7; console.log("showing 7") }
+  if (sessionStorage.getItem("Person_Name") == "Viktor Nikiforov"){ personPicked = person8; console.log("showing 8") }
+
+  // load their profile pic
+  pic_obj = document.getElementById("profile_pic");
+  console.log(personPicked.picture);
+  pic_obj.src = personPicked.picture;
+
+  skillNum = sessionStorage.getItem("Skill_Num");
+  skillInt = parseInt(skillNum) + 1;
 
 
-/*function populateExpertsList(skillNum) {
-  
-  let listIndex = 0;
-  
-  for (var i = 0; i < peopleArray.length; i++){ // iterating through peopleArray
-    
-    var skillCheck = 0;                         // person doesn't have skill
-    
-    if (skillNum == 1){
-      peopleArray[i].printing3D
-    }
+  //console.log(personPicked.getSkill(skillInt));
+
+  // load their name
+  element = document.getElementById("person_name");
+  element.innerText = text;
+
+  // load skill headline
+  element = document.getElementById("descrip_headline");
+
+  //console.log("skill num = " + skillNum);
+  element.innerText = "EXPERIENCE IN\n" + skillTitlesArray[skillNum];
+
+  // load skill description
+  element = document.getElementById("descrip_content");
+  element.innerText = personPicked.getSkill(skillInt);
+
+  // load status connect_status
+  space_obj2 = document.getElementById("connect_status");
+  // Displaying Connectory Status (Red / Green)
+  if (personPicked.status == 1){
+    space_obj2.style.background = 'rgb(40, 141, 0)';
+    space_obj2.innerText = "CURRENTLY IN THE CONNECTORY";
+    space_obj2.style.borderColor = 'rgb(40, 141, 0)';
+  } else {
+    space_obj2.style.background = 'rgb(208, 0, 0)';
+    space_obj2.innerText = "NOT IN THE CONNECTORY";
+    space_obj2.style.borderColor = 'rgb(208, 0, 0)';
   }
 }
 
-*/
