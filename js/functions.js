188c9af8624fd5e2c140c98268301cccb0bab7e7
AdrianaOlv15 <adriianaolv.24@gmail.com>
2023-04-10 03:01:02 -0500

    getting

--- js/functions.js
+++ js/functions.js
@@ -335,63 +335,75 @@ function loadProfile(){
 }
 
 
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
-        import { getDatabase, ref, set, child, update, remove, onValue } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
+/***** ..codeauthor:: Muthukumar Subramanian *****/
+/*****  HTML JavaScript to Write and Read on the Google Firebase *****/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+import { getDatabase, ref, set, child, update, remove, onValue } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
 
-        /***** Firebase config *****/
-        const firebaseConfig = {
-            apiKey: "AIzaSyC_jgj9zfAr5KIVQXlKPXby5Od2BOaTnGM",
-            authDomain: "rpi-mlx90614-821e6.firebaseapp.com",
-            databaseURL: "https://rpi-mlx90614-821e6-default-rtdb.firebaseio.com",
-            projectId: "rpi-mlx90614-821e6",
-            storageBucket: "rpi-mlx90614-821e6.appspot.com",
-            messagingSenderId: "213693056033",
-            appId: "1:213693056033:web:cca76584faaa68919b0f60"
-        };
-        
-        var db = getDatabase();
-        var number = 0;
-        var tbody = document.getElementById('tbody');
-        
-        // function to add a person record to the table 
-        function AddItemToTable(name, status){
-            
-            let trow = document.createElement("tr");    // creating a row
-            
-            let td1 = document.createElement('td');     // creating the column spaces for the row
-            let td2 = document.createElement('td');
-            let td3 = document.createElement('td');
-            
-            td1.innerHTML = ++number;                     // filling the column spaces
-            td2.innerHTML = name;
-            td3.innerHTML = status;
-           
-            trow.appendChild(td1);                      // linking the columns to the row
-            trow.appendChild(td2); 
-            trow.appendChild(td3); 
-            
-            tbody.appendChild(trow);                    // linking the row to the table
-        }
+/***** Firebase config *****/
+const firebaseConfig = {
+    apiKey: "AIzaSyC_jgj9zfAr5KIVQXlKPXby5Od2BOaTnGM",
+    authDomain: "rpi-mlx90614-821e6.firebaseapp.com",
+    databaseURL: "https://rpi-mlx90614-821e6-default-rtdb.firebaseio.com",
+    projectId: "rpi-mlx90614-821e6",
+    storageBucket: "rpi-mlx90614-821e6.appspot.com",
+    messagingSenderId: "213693056033",
+    appId: "1:213693056033:web:cca76584faaa68919b0f60"
+};
 
-        // function to add all the records to the table
-        function AddAllItemsToTable(TheUsers){
-            number = 0;
-            tbody.innerHTML=" ";
-            TheUsers.array.forEach(element => {
-                AddItemToTable(element.name, element.status);
-            });
-        }
+/***** Initialize Firebase *****/
+const app = initializeApp(firebaseConfig);
 
-        // get all data 
-        function GetAllDataOnce(){
-            const dbRef = ref(db);
-            get(child(dbRef,"SkillMateDatabase"))
-            .then((snapshot)=>{
-                var users = [];
-                snapshot.forEach(childSnapshot => {
-                    users.push(childSnapshot.val());
-                });
-                AddAllItemsToTable(users);
-            })
-        }
+/***** write data to firebase *****/
+function write_db() {
+    console.log("DEBUG: Write function");
+    var db = getDatabase();
+    var create_db_table = ref(db, 'level1/' + 'level2/');
+    var user_name = document.getElementById("user_name").value;
+    var user_password =  document.getElementById("user_name_password").value;
+    if( user_name == '' || user_password == ''){
+        alert("Make sure, must be non-empty data is required!!!");
+        console.log("Make sure, must be non-empty data is required!!!");
+        throw "Make sure, must be non-empty data is required!!!";
+    }
+    set(ref(db,'Check-in/'+user_name), {
+      status: user_password
+    }).then((res) => {
+        console.log();
+    })
+    .catch((err) => {
+        alert(err.message);
+        console.log(err.code);
+        console.log(err.message);
+    })
+}
+
+/***** read data from firebase *****/
+function read_db() {
+    var db = getDatabase();
+    var connect_db = ref(db, 'level1/' + 'level2/');
+    var retrieve_data='';
+    console.log("DEBUG: Read function");
+    onValue(connect_db, (snapshot) => {
+        retrieve_data = snapshot.val();
+        //console.log("user_name: " + retrieve_data.user_name);
+        //console.log("user_name_password: " + retrieve_data.user_name_password);
+        call_loop_print(retrieve_data);
+        document.getElementById("display_read_data").innerHTML =  "<pre>" + "user_name: " + retrieve_data.user_name +
+                '\n' + "user_name_password: " + retrieve_data.user_name_password + "</pre>";
+        })
+    function call_loop_print(retrieve_data){
+        for (var r=0;r<Object.entries(retrieve_data).length;r++){
+            var key = Object.keys(retrieve_data)[r];
+            var value = retrieve_data[key];
+            console.log("Key_" + r + ': ' + key + " Value_:" + r + ': ' + value );
+           }
+ }
+}
+/***** call write data function *****/
+var write_data_to_firebase = document.getElementById("write_data_to_firebase");
+write_data_to_firebase.addEventListener('click', write_db);
+
+/***** call read data function *****/
+var read_data_from_firebase = document.getElementById("read_data_from_firebase");
+read_data_from_firebase.addEventListener('click', read_db);
